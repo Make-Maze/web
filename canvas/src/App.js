1680445cc43b8e35c0c43b7bad9a620a9d789c80
yoosion030 <rsy5487@naver.com>
2021-12-16 15:27:23 +0900

    Add Route

--- canvas/src/App.js
+++ canvas/src/App.js
@@ -1,29 +1,40 @@
-import { Component } from 'react';
-import './App.css';
+import { Component } from 'react'
 import Canvas from './components/canvas'
+import Share from './components/Share'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 class App extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      select: "wall"
+      select: 'wall',
     }
   }
 
-  render(){
+  render() {
     return (
       <div className="App">
-        <div>
-          <Canvas select={this.state.select} onSelectChange={function(_select){
-            this.setState({
-              select: _select
-            })
-          }.bind(this)}></Canvas>
-        </div>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Canvas
+                  select={this.state.select}
+                  onSelectChange={function (_select) {
+                    this.setState({
+                      select: _select,
+                    })
+                  }.bind(this)}
+                ></Canvas>
+              }
+            ></Route>
+            <Route path="/share" element={<Share></Share>}></Route>
+          </Routes>
+        </BrowserRouter>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
