002d9b6b37c5a0dc616cc72194a15ac1d6e361a2
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2021-12-16 10:20:03 +0900

    12월 16일-데이터저장

--- canvas/src/App.js
+++ canvas/src/App.js
@@ -1,7 +1,6 @@
 import { Component } from 'react';
 import './App.css';
 import Canvas from './components/canvas'
-import Toolbar from './components/toolline';
 
 class App extends Component {
 
@@ -16,16 +15,12 @@ class App extends Component {
     return (
       <div className="App">
         <div>
-          <Canvas select={this.state.select}></Canvas>
-          <Toolbar select={this.state.select} onSelect={function(_select){
+          <Canvas select={this.state.select} onSelectChange={function(_select){
             this.setState({
               select: _select
             })
-          }.bind(this)}></Toolbar>
+          }.bind(this)}></Canvas>
         </div>
-        <button onClick={function(){
-          
-        }}>저장하기</button>
       </div>
     );
   }
--- canvas/src/components/canvas.js
+++ canvas/src/components/canvas.js
@@ -1,10 +1,14 @@
 import React, {useRef, useEffect, useState} from "react";
 import Block from './block'
 import SideBar from "./sidebar";
+import Toolbar from "./toolline";
+import domtoimage from "dom-to-image";
+import {saveAs} from "file-saver";
 
 const Canvas = (props) => {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
+  const blockRef = useRef(null);
 
   let list = new Array(20);
   for(let i = 0; i < 20; i++){
@@ -59,20 +63,34 @@ const Canvas = (props) => {
     }
   };
 
+  const downroad = () =>{
+    const canvas = blockRef.current;
+    const data = new FormData();
+    domtoimage.toBlob(canvas).then((blob) => {
+      data.append("file", blob)
+    });
+    data.append("array", new Blob([JSON.stringify(map)]))
+    for(let item of data.entries()){
+      console.log(item[0] + ", " + item[1]);
+    }
+  };
+
   return(
     <div>
       <div
       className="canvas_wrap"
       style={{position: "relative", display: "flex", width: window.innerWidth}}>
-        <canvas
-        ref={canvasRef}
-        style={{position: "absolute"}}
-        onMouseDown={startDrawing}
-        onMouseUp={finishDrawing}
-        onMouseMove={drawing}
-        onMouseLeave={finishDrawing}>
-        </canvas>
-        <Block x={x} y={y} isDrawing={isDrawing} map={map} width={width} height={height} select={props.select} delete={del} delMode={function(_del){setDel(_del)}} drawMode={drawMode}></Block>
+        <div ref={blockRef}>
+          <canvas
+          ref={canvasRef}
+          style={{position: "absolute"}}
+          onMouseDown={startDrawing}
+          onMouseUp={finishDrawing}
+          onMouseMove={drawing}
+          onMouseLeave={finishDrawing}>
+          </canvas>
+          <Block x={x} y={y} isDrawing={isDrawing} map={map} width={width} height={height} select={props.select} delete={del} delMode={function(_del){setDel(_del)}} drawMode={drawMode}></Block>
+        </div>
         <SideBar
          select={props.select}
          delet={del} 
@@ -83,8 +101,13 @@ const Canvas = (props) => {
           setDraw(_draw);
          }}></SideBar>
       </div>
+      <Toolbar select={props.select} onSelect={function(_select){
+        props.onSelectChange(_select);
+      }}></Toolbar>
+      <button onClick={function(){
+          downroad();
+        }}>저장하기</button>
     </div>
-    
   );
 }
 
