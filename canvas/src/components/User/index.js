12b1db79bb13be14891123ad0ac71af9cb574391
yoosion030 <rsy5487@naver.com>
2021-12-22 11:50:03 +0900

    Update Design

--- canvas/src/components/User/index.js
+++ canvas/src/components/User/index.js
@@ -3,12 +3,13 @@ import * as S from './style'
 import axios from 'axios'
 import { useResultContext } from '../../Context/Data'
 import Header from '../Header'
+import mainImg from '../../Assets/UserMainImg.png'
 
 const Share = () => {
   const [list, setList] = useState('')
   const [show, setShow] = useState()
 
-  const { data } = useResultContext()
+  const { data, title } = useResultContext()
   console.log(data)
   return (
     <>
@@ -20,7 +21,13 @@ const Share = () => {
             <p>이름 : {data.name}</p>
             <p>이메일 : {data.email}</p>
           </div>
+          <img src={mainImg} alt="" className="mainImg" />
         </S.UserSection>
+        <hr />
+        <S.MapSection>
+          <h1>자기가 저장한 미로랑 기타 정보</h1>
+          {title}
+        </S.MapSection>
       </S.MainSection>
     </>
   )
--- canvas/src/components/User/style.js
+++ canvas/src/components/User/style.js
@@ -1,20 +1,18 @@
 import styled from 'styled-components'
 
 export const MainSection = styled.div`
-  padding: 20vh 7vw 0px;
-
+  padding: 20vh 7vw 20vw;
   * {
     margin: 0;
-  }
-  h1 {
-    font-size: 60px;
-    margin-bottom: 100px;
+    overflow-x: hidden;
   }
 `
 
 export const UserSection = styled.div`
+  position: relative;
   display: flex;
   align-items: center;
+  padding-bottom: 300px;
   img {
     width: 300px;
     height: 300px;
@@ -22,6 +20,24 @@ export const UserSection = styled.div`
   }
   p {
     padding-left: 130px;
-    font-size: 50px;
+    font-size: 24px;
+    margin-bottom: 20px;
+  }
+  .mainImg {
+    position: absolute;
+    width: 1000px;
+    height: 528px;
+    top: 20px;
+    left: 800px;
+  }
+  hr {
+    width: 5px;
+  }
+`
+
+export const MapSection = styled.div`
+  h1 {
+    margin-top: 100px;
+    font-size: 60px;
   }
 `
