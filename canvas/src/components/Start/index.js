bae3d0822208f7f1cee8d608134533bc9bbe36d0
yoosion030 <rsy5487@naver.com>
2021-12-23 10:06:57 +0900

    Upedate googlelogin

--- canvas/src/components/Start/index.js
+++ canvas/src/components/Start/index.js
@@ -2,53 +2,44 @@ import React, { useDebugValue, useState, useNavigate } from 'react'
 import { Link } from 'react-router-dom'
 import * as S from './style'
 import Footer from '../../Assets/FooterImg.png'
-import GoogleLogin from '../../Assets/GoogleLogin.png'
+import GoogleLoginImg from '../../Assets/GoogleLogin.png'
 import GuestLogin from '../../Assets/GuestLogin.png'
 import axios from 'axios'
+import GoogleLogin from 'react-google-login'
 
 import { useResultContext } from '../../Context/Data'
 const Start = () => {
   const { data, setData } = useResultContext()
+  const clientId =
+    '121704372282-6l10fcfppqtqgbhr3mk9guacs6r63pcl.apps.googleusercontent.com'
+  // axios({
+  //   method: 'get',
+  //   url: '/oauth/googleRequest',
+  // })
+  //   .then(res => {
+  //     console.log(res)
+  //     setData({
+  //       email: res.data.email,
+  //       name: res.data.name,
+  //       img: res.data.picture,
+  //     })
+  //     localStorage.setItem('m&m_token', res.data.access_token)
+  //   })
+  //   .catch(err => console.log(err))
 
-  const TryLogin = response => {
-    // axios({
-    //   method: 'get',
-    //   url: '/oauth/googleRequest',
-    // })
-    //   .then(res => {
-    //     console.log(res)
-    //     setData({
-    //       email: res.data.email,
-    //       name: res.data.name,
-    //       img: res.data.picture,
-    //     })
-    //     localStorage.setItem('m&m_token', res.data.access_token)
-    //   })
-    //   .catch(err => console.log(err))
-
-    async function TryLogin(response) {
-      try {
-        const res = await axios.get('/oauth/googleRequest')
-        setData({
-          email: res.data.email,
-          name: res.data.name,
-          img: res.data.picture,
-        })
-        console.log(res)
-      } catch (err) {
-        console.log(err)
-      }
+  const onSuccess = response => {
+    const userData = {
+      profileImage: response.profileObj.imageUrl,
+      email: response.profileObj.email,
+      name: response.profileObj.name,
+      // accessToken = response
     }
+    console.log(userData)
   }
-
+  const onFailure = error => {
+    console.log(error)
+  }
   const guestLogin = response => {}
-  // async function Login() {
-  //   try {
-  //     await
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
 
   return (
     <S.MainSection>
@@ -62,8 +53,17 @@ const Start = () => {
             <S.Green>맵 제작</S.Green> 및 <S.Green>플레이</S.Green>을 하고
             싶으시다면
           </S.Text>
-          <S.LoginBtn onClick={TryLogin}>Google 로그인</S.LoginBtn>
-          <img src={GoogleLogin} />
+          <GoogleLogin
+            buttonText="구글 로그인"
+            accessType="offline"
+            responseType="permission"
+            approvalPrompt="force"
+            prompt="consent"
+            clientId={clientId}
+            onSuccess={onSuccess}
+            onFailure={onFailure}
+          ></GoogleLogin>{' '}
+          <img src={GoogleLoginImg} />
         </S.LoginSection>
         <S.LoginSection>
           <S.Text>
