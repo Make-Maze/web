a7d71cbc4ccb41d22c9c8157cc682d55ed367038
yoosion030 <rsy5487@naver.com>
2021-12-23 16:26:53 +0900

    Change function component

--- canvas/src/App.js
+++ canvas/src/App.js
@@ -6,23 +6,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ResultContextProvider } from './Context/Data' //provider 불러오기
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-class App extends Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //     select: 'wall',
-  //   }
-  // }
 
-  render() {
-    return (
-      <div className="App">
-        <ResultContextProvider>
-          <BrowserRouter>
-            <ToastContainer />
-            <Routes>
-              <Route path="/" element={<P.StartPage></P.StartPage>}></Route>
-              {/* <Route
+const App = () => {
+  return (
+    <div className="App">
+      <ResultContextProvider>
+        <BrowserRouter>
+          <ToastContainer />
+          <Routes>
+            <Route path="/" element={<P.StartPage></P.StartPage>}></Route>
+            {/* <Route
                 path="/Draw"
                 element={
                   <C.Draw
@@ -35,22 +28,18 @@ class App extends Component {
                   ></C.Draw>
                 }
               ></Route> */}
-              <Route path="/Draw" element={<P.DrawPage></P.DrawPage>}></Route>
-              <Route path="/User" element={<P.UserPage></P.UserPage>}></Route>
-              <Route
-                path="/Share"
-                element={<P.SharePage></P.SharePage>}
-              ></Route>
-              <Route
-                path="/*"
-                element={<P.NotFoundPage></P.NotFoundPage>}
-              ></Route>
-            </Routes>
-          </BrowserRouter>
-        </ResultContextProvider>
-      </div>
-    )
-  }
+            <Route path="/Draw" element={<P.DrawPage></P.DrawPage>}></Route>
+            <Route path="/User" element={<P.UserPage></P.UserPage>}></Route>
+            <Route path="/Share" element={<P.SharePage></P.SharePage>}></Route>
+            <Route
+              path="/*"
+              element={<P.NotFoundPage></P.NotFoundPage>}
+            ></Route>
+          </Routes>
+        </BrowserRouter>
+      </ResultContextProvider>
+    </div>
+  )
 }
 
 export default App
