d9a83e3ba963c001a0ac220d5c810a2f475b49f4
yoosion030 <rsy5487@naver.com>
2021-12-23 14:53:04 +0900

    Add NotFoundPage

---
+++ canvas/src/components/NotFound/style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const MainSection = styled.div`
+  color: #1b2735;
+  height: 100vh;
+  /* background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%); */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    font-size: 150px;
+  }
+  a {
+    line-height: 60px;
+  }
+  a:hover {
+    color: #1b2735;
+  }
+`
