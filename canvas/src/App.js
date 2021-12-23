a88082b19357181c41bb231263f1f38bb997ceab
yoosion030 <rsy5487@naver.com>
2021-12-23 17:40:10 +0900

    비회원 상태에서 주소를 요청할 때 404페이지 처리

--- canvas/src/App.js
+++ canvas/src/App.js
@@ -1,43 +1,42 @@
-import { Component } from 'react'
 import './App.css'
-import * as C from './components'
 import * as P from './Pages'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { ResultContextProvider } from './Context/Data' //provider 불러오기
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { useResultContext } from './Context/Data'
 
 const App = () => {
+  const { isLogin } = useResultContext()
   return (
     <div className="App">
-      <ResultContextProvider>
-        <BrowserRouter>
-          <ToastContainer />
-          <Routes>
-            <Route path="/" element={<P.StartPage></P.StartPage>}></Route>
-            {/* <Route
-                path="/Draw"
-                element={
-                  <C.Draw
-                    select={this.state.select}
-                    onSelectChange={function (_select) {
-                      this.setState({
-                        select: _select,
-                      })
-                    }.bind(this)}
-                  ></C.Draw>
-                }
-              ></Route> */}
-            <Route path="/Draw" element={<P.DrawPage></P.DrawPage>}></Route>
-            <Route path="/User" element={<P.UserPage></P.UserPage>}></Route>
-            <Route path="/Share" element={<P.SharePage></P.SharePage>}></Route>
-            <Route
-              path="/*"
-              element={<P.NotFoundPage></P.NotFoundPage>}
-            ></Route>
-          </Routes>
-        </BrowserRouter>
-      </ResultContextProvider>
+      <BrowserRouter>
+        <ToastContainer />
+        <Routes>
+          {isLogin ? (
+            <>
+              <Route path="/" element={<P.StartPage></P.StartPage>}></Route>
+              <Route path="/Draw" element={<P.DrawPage></P.DrawPage>}></Route>
+              <Route path="/User" element={<P.UserPage></P.UserPage>}></Route>
+              <Route
+                path="/Share"
+                element={<P.SharePage></P.SharePage>}
+              ></Route>
+              <Route
+                path="/*"
+                element={<P.NotFoundPage></P.NotFoundPage>}
+              ></Route>
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<P.StartPage></P.StartPage>}></Route>
+              <Route
+                path="/*"
+                element={<P.NotFoundPage></P.NotFoundPage>}
+              ></Route>
+            </>
+          )}
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- canvas/src/components/NotFound/index.js
+++ canvas/src/components/NotFound/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Snowfall from 'react-snowfall'
+import { useResultContext } from '../../Context/Data'
 import * as S from './style'
 const NotFound = () => {
+  const { isLogin } = useResultContext()
   return (
     <S.MainSection>
       <Snowfall />
       <h1>Not Found</h1>
       찾을 수 없는 페이지 입니다.
-      <Link to="/">홈으로 이동</Link>
+      {isLogin ? (
+        <Link to="/Draw">홈으로 이동</Link>
+      ) : (
+        <Link to="/">홈으로 이동</Link>
+      )}
     </S.MainSection>
   )
 }
