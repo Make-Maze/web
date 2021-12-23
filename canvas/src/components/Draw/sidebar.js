3779180330d1b19ed877fa11f50fa1aeea4c9cbe
yoosion030 <rsy5487@naver.com>
2021-12-23 09:46:51 +0900

    Update

--- canvas/src/components/Draw/sidebar.js
+++ canvas/src/components/Draw/sidebar.js
@@ -45,7 +45,7 @@ const SideBar = props => {
     ></div>
   )
 
-  return <div className="sidebar">{props.item ? 'item' : 'sideBar'}</div>
+  return <div className="sidebar">{props.item ? 'item' : _content}</div>
 }
 
 export default SideBar
