298b6a554c00eec2688b82fcc280684b13d1de88
yoosion030 <rsy5487@naver.com>
2022-03-27 10:26:56 +0900

    Add onClick Event

--- canvas/src/components/Button/index.js
+++ canvas/src/components/Button/index.js
@@ -2,7 +2,12 @@ import * as S from "./style";
 
 const Button = (props) => {
   const content = props.content;
-  return <S.Button>{content}</S.Button>;
+  const onClick = props.onClick;
+  return (
+    <span onClick={onClick}>
+      <S.Button>{content}</S.Button>
+    </span>
+  );
 };
 
 export default Button;
--- canvas/src/components/Button/style.js
+++ canvas/src/components/Button/style.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Button = styled.div`
+  display: inline-block;
   width: 100px;
   height: 45px;
   border: none;
